fd4bdf0c5210666ce24f801113c45694f9fc6889
maty5302 <103773386+maty5302@users.noreply.github.com>
2024-12-10 23:42:58 +0100

    Adding ReservationControllers.cs

---
+++ SuperReservationSystem/Controllers/ReservationController.cs
@@ -0,0 +1,123 @@
+﻿using ApiCisco;
+using BusinessLayer.Models;
+using BusinessLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+using SuperReservationSystem.Models;
+
+namespace SuperReservationSystem.Controllers
+{
+    public class ReservationController : Controller
+    {
+        ServerService serverService = new ServerService();
+        UserService userService = new UserService();
+        ReservationService reservationService = new ReservationService();
+
+        public IActionResult Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Login");
+
+            var reservations = reservationService.GetAllReservations();
+
+            List<ReservationInformationModel> list = new List<ReservationInformationModel>();
+            foreach (var reservation in reservations)
+            {
+                var server = serverService.GetServerById(reservation.ServerId);
+                var user = userService.GetUsername(reservation.UserId);
+                list.Add(new ReservationInformationModel
+                {
+                    Id = reservation.Id,
+                    ServerName = server.Name,
+                    ServerType = server.ServerType,
+                    ServerId = server.Id,
+                    LabId = reservation.LabId,
+                    ReservationStart = reservation.ReservationStart,
+                    ReservationEnd = reservation.ReservationEnd,
+                    UserId = reservation.UserId,
+                    UserName = user
+                });
+            }
+
+            ViewBag.Reservations = list;
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> Create(ReservationModel model, int? selectedServer)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Login");
+            var servers = serverService.GetAllServers();
+            ViewBag.Servers = servers;
+            ViewBag.Labs3 = new string[0]; // Initialize with empty array to avoid null reference
+
+            if (selectedServer.HasValue)
+            {
+                model.ServerId = selectedServer.Value;
+                var selectedServerModel = serverService.GetServerById(selectedServer.Value);
+                var userHttpClient = new UserHttpClient(selectedServerModel.IpAddress);
+                var res = await Authentication.Authenticate(userHttpClient, selectedServerModel.Username, selectedServerModel.Password);
+                if (res.IsSuccessStatusCode) {
+                    var labs = await Lab.GetLabs(userHttpClient);
+
+                    if (labs != null && labs.Length > 0)
+                    {
+                        ViewBag.Labs3 = labs;
+                    }
+                }
+            }
+
+            return View("Create", model);
+        }
+
+
+        [HttpPost]
+        public IActionResult MakeReservation(ReservationModel reserve, int? selectedServer)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Login");
+            var UserId = userService.GetUserId(User.Identity.Name);
+            if (reserve.ReservationStart >= reserve.ReservationEnd)
+            {
+                TempData["ErrorMessage"] = "Invalid reservation time.";
+                return RedirectToAction("Create");
+            }
+            if (reserve.ReservationStart < DateTime.Now)
+            {
+                TempData["ErrorMessage"] = "Invalid reservation time.";
+                return RedirectToAction("Create");
+            }
+            if (!selectedServer.HasValue && selectedServer == 0)
+            {
+                TempData["ErrorMessage"] = "Server not selected.";
+                return RedirectToAction("Create");
+            }
+            if (reserve.LabId == null)
+            {
+                TempData["ErrorMessage"] = "Lab not selected.";
+                return RedirectToAction("Create");
+            }
+            if (reserve.UserId == -1)
+            {
+                TempData["ErrorMessage"] = "User not selected.";
+                return RedirectToAction("Create");
+            }
+            var reservation = new ReservationModel
+            {
+                ServerId = selectedServer.Value,
+                LabId = reserve.LabId,
+                ReservationStart = reserve.ReservationStart,
+                ReservationEnd = reserve.ReservationEnd,
+                UserId = UserId
+            };
+            var result = reservationService.MakeReservation(reservation);
+            if (result)
+                TempData["SuccessMessage"] = "Reservation created.";
+            else
+                TempData["ErrorMessage"] = "Something went wrong. See log.";
+            return RedirectToAction("Index","Reservation");
+        }
+
+    }
+}
---
+++ SuperReservationSystem/Models/ReservationInformationModel.cs
@@ -0,0 +1,11 @@
+﻿using BusinessLayer.Models;
+
+namespace SuperReservationSystem.Models
+{
+    public class ReservationInformationModel : ReservationModel
+    {
+        public required string ServerName { get; set; }
+        public required string ServerType { get; set; }
+        public required string UserName { get; set; }
+    }
+}
