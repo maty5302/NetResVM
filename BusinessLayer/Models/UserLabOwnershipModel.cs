c709be0410255610b6174844ea88a49eb023a831
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-12 21:15:41 +0100

    Adding Model for UserLabOwnership

---
+++ BusinessLayer/Models/UserLabOwnershipModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class UserLabOwnershipModel
+    {
+        public int UserId { get; set; }
+        public int ServerId { get; set; }
+        public required string LabId { get; set; }
+    }
+}
