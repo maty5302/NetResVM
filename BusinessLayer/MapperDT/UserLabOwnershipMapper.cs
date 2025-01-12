0c8c57f84c0ea1563486f0a824d3e4b35205151b
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-12 21:15:21 +0100

    Adding Mapper for UserLabOwnershipService

---
+++ BusinessLayer/MapperDT/UserLabOwnershipMapper.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.MapperDT
+{
+    public class UserLabOwnershipMapper
+    {
+        public static UserLabOwnershipModel Map(DataRow row)
+        {
+            return new UserLabOwnershipModel
+            {
+                UserId = (int)row["UserID"],
+                LabId = (string)row["LabID"],
+                ServerId = (int)row["ServerID"]
+            };
+        }
+    }
+}
