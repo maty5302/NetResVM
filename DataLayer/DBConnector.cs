e93bf77852b01da26e3a9fba26097fe345755430
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-12 18:58:51 +0100

    Logging in DBConnector.cs if database connection settings are invalid

--- DataLayer/DBConnector.cs
+++ DataLayer/DBConnector.cs
@@ -5,26 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using SimpleLogger;
 
 namespace DataLayer
 {
     internal class DBConnector
     {
+        static ILogger _logger = FileLogger.Instance;
         public static SqlConnection GetConnection()
         {
             var builder = GetBuilder();
             if (builder == null || string.IsNullOrEmpty(builder.UserID) || string.IsNullOrEmpty(builder.Password) || string.IsNullOrEmpty(builder.DataSource))
             {
-                throw new DatabaseConfigurationException("Database connection settings are missing or invalid.");
+                _logger.LogError("Database connection settings are missing or invalid.");
+            
+                throw new DatabaseConfigurationException("Database connection settings are missing or invalid.");                
             }
-
+            
             return new SqlConnection(builder.ConnectionString);
         }
 
-        public static SqlConnectionStringBuilder? GetBuilder()
+        private static SqlConnectionStringBuilder? GetBuilder()
         {
             var file = JsonSerializer.Deserialize<Dictionary<string,string>>(File.ReadAllText("sqlconnection.json"));
-            if (file.TryGetValue("DataSource", out var dataSourceValue) && file.TryGetValue("UserID", out var userIdValue) && file.TryGetValue("Password", out var passValue))
+            if (file != null && file.TryGetValue("DataSource", out var dataSourceValue) && file.TryGetValue("UserID", out var userIdValue) && file.TryGetValue("Password", out var passValue))
             {
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
                 builder.DataSource = dataSourceValue;
