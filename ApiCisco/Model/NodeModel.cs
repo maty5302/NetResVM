ee4f536cf45248748e984fa9b73a8346f3e5ab7b
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-13 17:14:59 +0100

    Adding NodeModel and Node.cs - Function for getting all nodes - Function for getting information about node

---
+++ ApiCisco/Model/NodeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ApiCisco.Model
+{
+    public class NodeModel
+    {
+        [JsonPropertyName("id")]
+        public required string Id { get; set; }
+        [JsonPropertyName("label")]
+        public required string Name { get; set; }
+        [JsonPropertyName("state")]
+        public required string State { get; set; }
+        [JsonPropertyName("cpus")]
+        public int? Cpu { get; set; }
+        [JsonPropertyName("cpu_limit")]
+        public int? CpuLimit { get; set; }
+        [JsonPropertyName("ram")]
+        public int? Memory { get; set; }
+        [JsonPropertyName("data_volume")]
+        public int? DataVolume { get; set; }
+    }
+}
---
+++ ApiCisco/Node.cs
@@ -0,0 +1,67 @@
+﻿using ApiCisco.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiCisco
+{
+    public static class Node
+    {
+        public static async Task<string[]?> GetNodes(UserHttpClient user, string labId)
+        {
+            var url = user.Url + $"labs/{labId}/nodes";
+            try
+            {
+                var response = await user.Client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result.Split(",");
+                    string[] charactersToRemove = { "\n", "\"", "[", "]", " " };
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        foreach (string item in charactersToRemove)
+                        {
+                            data[i] = data[i].Replace(item, string.Empty);
+                        }
+                    }
+                    return data;
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+        public static async Task<NodeModel?> GetNodeInfo(UserHttpClient user,string labId, string nodeId)
+        {
+            var url = user.Url + $"labs/{labId}/nodes/{nodeId}";
+            try
+            {
+                var response = await user.Client.GetAsync(url.Trim());
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result;
+                    var node = JsonSerializer.Deserialize<NodeModel>(data);
+                    return node;
+                }
+                else if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("Node not found");
+                    return null;
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
