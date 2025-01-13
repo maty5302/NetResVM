13904cc5fa5609ee81397ccb78a83921aed15cf2
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-13 17:14:07 +0100

    Minor changes to Lab.cs - adding stopAllLabs function - passing message in StartLab

--- ApiCisco/Lab.cs
+++ ApiCisco/Lab.cs
@@ -27,7 +27,7 @@ namespace ApiCisco
                         foreach (string item in charactersToRemove)
                         {
                             data[i] = data[i].Replace(item, string.Empty);
-                        }
+                        }                           
                     }
                     return data;
                 }
@@ -43,65 +43,79 @@ namespace ApiCisco
 
         public static async Task<LabModel?> GetLabInfo(UserHttpClient user, string labId)
         {
-            var url = user.Url + "labs/" + labId.Trim();
+            var url = $"{user.Url}labs/{labId.Trim()}";
             try
             {
-                var response = user.Client.GetAsync(url.Trim()).Result;
+                var response = await user.Client.GetAsync(url.Trim());
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
                     var lab = JsonSerializer.Deserialize<LabModel>(data);
-                    return await Task.FromResult<LabModel?>(lab);
+                    return lab;
                 }
                 else if (response.StatusCode == HttpStatusCode.NotFound)
                 {
                     Console.WriteLine("Lab not found");
-                    return await Task.FromResult<LabModel?>(null);
+                    return null;
                 }
-                return await Task.FromResult<LabModel?>(null);
+                return null;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                return await Task.FromResult<LabModel?>(null);
+                return null;
             }
         }
 
-        public static async Task<string> DownloadLab(UserHttpClient user, string labId)
+        public static async Task<string?> DownloadLab(UserHttpClient user, string labId)
         {
             var url = user.Url + "labs/" + labId.Trim() + "/download";
             try
             {
-                var response = user.Client.GetAsync(url.Trim()).Result;
+                var response = await user.Client.GetAsync(url.Trim());
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
-                    return await Task.FromResult<string>(data);
+                    return data;
                 }
                 else if (response.StatusCode == HttpStatusCode.NotFound)
                 {
                     Console.WriteLine("Lab not found");
-                    return await Task.FromResult<string>("Lab not found");
+                    return null;
                 }
-                return await Task.FromResult<string>("An error occurred");
+                return null;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                return await Task.FromResult<string>("An error occurred");
+                return null;
             }
         }
 
-        public static async Task<bool> StartLab(UserHttpClient user, string labId)
+        public static async Task<(bool,string)> StartLab(UserHttpClient user, string labId)
         {
+            var labs = GetLabs(user).Result;
+            if (labs == null || !labs.Contains(labId))
+                return (false,"Lab not found");
+            foreach (var item in labs)
+            {
+                var labInfo = GetLabInfo(user, item);
+                if (labInfo.Result != null)
+                {
+                    if (labInfo.Result.Id == labId && labInfo.Result.State.ToLower() == "started")
+                        return (true,"");
+                    else if (labInfo.Result.State.ToLower() == "started")
+                        return (false, "Another lab is already running..");
+                }
+            }
             var url = user.Url + "labs/" + labId.Trim() + "/start";
             try
             {
                 var response = await user.Client.PutAsync(url.Trim(), null);
                 if (response.StatusCode == HttpStatusCode.NoContent)
-                    return true;
+                    return (true, "Lab started successfully");
                 else
-                    return false;
+                    return (false, response.StatusCode.ToString());
             }
             catch (Exception ex)
             {
@@ -123,6 +137,32 @@ namespace ApiCisco
             catch (Exception ex)
             {
                 throw new Exception($"Failed to stop the lab with ID {labId}.", ex);
+            }
+        }
+
+        public static async Task<bool> StopAllLabs(UserHttpClient user)
+        {
+            var labs = GetLabs(user).Result;          
+            try
+            {
+                if (labs != null)
+                {
+                    foreach (var item in labs)
+                    {
+                        var url = user.Url + "labs/" + item.Trim() + "/stop";
+                        var response = await user.Client.PutAsync(url.Trim(), null);
+                        if (response.StatusCode == HttpStatusCode.NoContent)
+                            continue;
+                        else
+                            return false;
+                    }
+                    return true;
+                }
+                return false;
+            }   
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to stop all labs.", ex);
             }
         }
 
