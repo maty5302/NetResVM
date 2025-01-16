2c2dfc4d1498460834f6e5a2712587d307a10f94
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-17 00:53:11 +0100

    Updating UserMapper.cs and UserModel.cs - reflecting recent change where we map even if user is active or not

--- BusinessLayer/MapperDT/UserMapper.cs
+++ BusinessLayer/MapperDT/UserMapper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace BusinessLayer.MapperDT
 {
     public class UserMapper
     {
+
+        private static bool GetActiveRow(DataRow row)
+        {
+            if (row["Active"].ToString() == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         public static UserModel Map(DataRow row)
         {
             return new UserModel
@@ -17,7 +31,8 @@ namespace BusinessLayer.MapperDT
                 Id = (int)row["UserID"],
                 Username = (string)row["Username"],
                 Password = (string)row["Password"],
-                Role = (string)row["Role"]
+                Role = (string)row["Role"],
+                Active = GetActiveRow(row)
             };
         }
     }
--- BusinessLayer/Models/UserModel.cs
+++ BusinessLayer/Models/UserModel.cs
@@ -10,7 +10,9 @@ namespace BusinessLayer.Models
     {
         public required int Id { get; set; }
         public required string Username { get; set; }
-        public required string Password { get; set; }
+        public string? Password { get; set; } //password could be null if user is remote
         public required string Role { get; set; }
+        // public required string AuthorizationType { get; set; } // will be localhost or remote
+        public bool Active { get; set; } // will be true or false
     }
 }
