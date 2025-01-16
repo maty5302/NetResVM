d67694695eb75baabe15c6594476221e8e421d07
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-17 00:48:44 +0100

    Updating UserTableDataGateway.cs
    - Adding CRUD Methods for Users
    - A new method GetAllUsers has been added, which returns all users from the "User" table.
    - A new method AddUser has been added, which allows adding a new user to the "User" table with parameters Username, Password, Role, and Active.
    - A new method UpdateUserActive has been added, which allows updating the Active attribute for a user with a given Id.
    - A new method UpdateUserPassword has been added, which allows updating the password for a user with a given Id.
    - A new method RemoveUser has been added, which allows removing a user with a given Id from the "User" table.

--- DataLayer/UserTableDataGateway.cs
+++ DataLayer/UserTableDataGateway.cs
@@ -30,7 +30,7 @@ namespace DataLayer
             return result;
         }
 
-        public DataTable GetUserById(int id)
+        public DataTable GetUserById(int Id)
         {
             string query = "SELECT * FROM \"User\" WHERE UserID = @Id";
             var result = new DataTable();
@@ -39,7 +39,7 @@ namespace DataLayer
                 connection.Open();
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@Id", Id);
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         result.Load(reader);
@@ -47,6 +47,87 @@ namespace DataLayer
                 }
             }
             return result;
+        }
+
+        public DataTable GetAllUsers()
+        {
+            string query = "SELECT * FROM \"User\"";
+            var result = new DataTable();
+            using (var connection = DBConnector.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        result.Load(reader);
+                    }
+                }
+            }
+            return result;
+        }
+
+        //In future AuthorizationType will be added
+        public void AddUser(string Username, string Password, string Role, int Active)
+        {
+            string query = "INSERT INTO \"User\" (Username, Password, Role, Active) VALUES (@username, @password, @role, @active)";
+            using (var connection = DBConnector.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", Username);
+                    command.Parameters.AddWithValue("@password", Password);
+                    command.Parameters.AddWithValue("@role", Role);
+                    command.Parameters.AddWithValue("@active", Active);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        //Update method for Active atribute
+        public void UpdateUserActive(int Id, int Active)
+        {
+            string query = "UPDATE \"User\" SET Active = @active WHERE UserID = @Id";
+            using (var connection = DBConnector.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@active", Active);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateUserPassword(int Id, string Password)
+        {
+            string query = "UPDATE \"User\" SET Password = @password WHERE UserID = @Id";
+            using (var connection = DBConnector.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@password", Password);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void RemoveUser(int Id)
+        {
+            string query = "DELETE FROM \"User\" WHERE UserID = @Id";
+            using (var connection = DBConnector.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
