ad4e1c1b230eff0b3d8110c5d0a0d23aeaba99c6
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-19 17:38:13 +0100

    Updating BackgroundTask.cs - Adding delete of expired reservation after 6 months - Adding functionality of stopping all labs before running new reserved lab - Adding logger - Fixing issues where null would crash app

--- SuperReservationSystem/BackgroundTask.cs
+++ SuperReservationSystem/BackgroundTask.cs
@@ -1,5 +1,6 @@
 ﻿using ApiCisco;
 using BusinessLayer.Services;
+using SimpleLogger;
 using System.Threading.Tasks;
 
 namespace SuperReservationSystem
@@ -7,35 +8,63 @@ namespace SuperReservationSystem
     public class BackgroundTask
     {
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-        private ReservationService _reservationService = new ReservationService();
-        private ServerService _serverService = new ServerService();
+        private readonly ReservationService _reservationService = new ReservationService();
+        private readonly ServerService _serverService = new ServerService();
 
-        public async void CheckReservations()
+        private async void CheckReservations()
         {
-            var reservations = _reservationService.GetAllReservations();
-            var time = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, 0);
-            Console.WriteLine($"Checking reservations..{time}");
-            foreach (var reservation in reservations)
+            try
             {
-                var server = _serverService.GetServerById(reservation.ServerId);
-                UserHttpClient client = new UserHttpClient(server.IpAddress);
-                //if (reservation.ReservationEnd < DateTime.Now)
-                //{
-                //    _reservationService.DeleteReservation(reservation.Id);
-                //}
-                if (reservation.ReservationStart == time && server !=null && server.ServerType=="CML")
+                var reservations = _reservationService.GetAllReservations();
+                var time = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, 0);
+                FileLogger.Instance.Log($"Checking reservations..{time}");
+                if (reservations != null)
                 {
-                    await Authentication.Authenticate(client, server.Username, server.Password);
-                    var res = await Lab.StartLab(client, reservation.LabId);
-                    Console.WriteLine(res.ToString());
-                }
-                else if(reservation.ReservationEnd==time && server != null && server.ServerType == "CML")
-                {
-                    await Authentication.Authenticate(client, server.Username, server.Password);
-                    var res = await Lab.StopLab(client, reservation.LabId);
-                    Console.WriteLine(res.ToString());
+                    foreach (var reservation in reservations)
+                    {
+                        var server = _serverService.GetServerById(reservation.ServerId);
+                        if (server == null)
+                        {
+                            FileLogger.Instance.LogError("Server not found.");
+                            continue;
+                        }
 
+                        UserHttpClient client = new UserHttpClient(server.IpAddress); 
+                        if (reservation.ReservationEnd.AddMonths(6) < DateTime.Now)
+                        {
+                            _reservationService.DeleteReservation(reservation.Id);
+                        }
+
+                        if (reservation.ReservationStart == time && server.ServerType == "CML") //reservation.ReservationStart<time && reservation.ReservationEnd > time && //lab not running?
+                        {
+                            await Authentication.Authenticate(client, server.Username, server.Password);
+                            var stopped = await Lab.StopAllLabs(client);
+                            if (stopped)
+                            {
+                                var res = await Lab.StartLab(client, reservation.LabId);
+                                if (res.Item1)
+                                    FileLogger.Instance.Log("Lab started");
+                                else
+                                    FileLogger.Instance.LogWarning("Lab could not be started");
+                            }
+                            else
+                                FileLogger.Instance.LogWarning("Labs could not be stopped");
+                        }
+                        else if (reservation.ReservationEnd <= time && server.ServerType == "CML")
+                        {
+                            await Authentication.Authenticate(client, server.Username, server.Password);
+                            var res = await Lab.StopLab(client, reservation.LabId);
+                            if (res)
+                                FileLogger.Instance.Log("Lab stopped");
+                            else
+                                FileLogger.Instance.LogWarning("Lab could not be stopped");
+                        }
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                FileLogger.Instance.LogError(e.Message);
             }
         }
 
@@ -46,18 +75,18 @@ namespace SuperReservationSystem
                 while (!_cancellationTokenSource.Token.IsCancellationRequested)
                 {
                     try
-                    {
+                    {   // Check reservations every minute
                         CheckReservations();
                         await Task.Delay(TimeSpan.FromMinutes(1), _cancellationTokenSource.Token);
                     }
                     catch (TaskCanceledException)
-                    {
-                        // Ignore
+                    {                        
+                        FileLogger.Instance.LogWarning("Task was cancelled");
                     }
                     catch (Exception ex)
                     {
                         // Log or handle exceptions
-                        Console.WriteLine($"Error: {ex.Message}");
+                        FileLogger.Instance.LogError(ex.Message);
                     }
                 }
             },_cancellationTokenSource.Token);
@@ -65,7 +94,9 @@ namespace SuperReservationSystem
 
         public void Stop()
         {
+            // Cancel the task
             _cancellationTokenSource.Cancel();
+            FileLogger.Instance.LogWarning("Background task for checking reservations was cancelled.");
         }
 
     }
