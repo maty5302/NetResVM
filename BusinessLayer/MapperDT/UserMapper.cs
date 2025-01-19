9df86b03b95cb5a21474e8cd8afddb68e2cc557f
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-01-19 19:52:35 +0100

    Updating UserModel.cs and UserMapper.cs - Adding AuthorizationType required string - Added mapping of AuthorizationType and simplified mapping of Active

--- BusinessLayer/MapperDT/UserMapper.cs
+++ BusinessLayer/MapperDT/UserMapper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Dynamic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,28 +12,16 @@ namespace BusinessLayer.MapperDT
 {
     public class UserMapper
     {
-
-        private static bool GetActiveRow(DataRow row)
-        {
-            if (row["Active"].ToString() == "1")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
         public static UserModel Map(DataRow row)
         {
             return new UserModel
             {
                 Id = (int)row["UserID"],
                 Username = (string)row["Username"],
-                Password = (string)row["Password"],
+                Password = row["Password"] is DBNull ? null : row["Password"].ToString(),
                 Role = (string)row["Role"],
-                Active = GetActiveRow(row)
+                AuthorizationType = (string)row["AuthorizationType"],
+                Active = row["Active"] is 1 ? true : false
             };
         }
     }
--- BusinessLayer/Models/UserModel.cs
+++ BusinessLayer/Models/UserModel.cs
@@ -10,9 +10,9 @@ namespace BusinessLayer.Models
     {
         public required int Id { get; set; }
         public required string Username { get; set; }
-        public string? Password { get; set; } //password could be null if user is remote
-        public required string Role { get; set; }
-        // public required string AuthorizationType { get; set; } // will be localhost or remote
+        public string? Password { get; set; } //password could be null if user is vsb
+        public required string Role { get; set; } // admin or student
+        public required string AuthorizationType { get; set; } // will be localhost or vsb
         public bool Active { get; set; } // will be true or false
     }
 }
