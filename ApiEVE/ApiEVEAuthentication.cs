2604dcb00d3bb0877566d449197f1695f6eec850
Matěj Prudil <matej.prudil@outlook.cz>
2025-01-26 18:25:12 +0100

    Adding ApiEVEHttpClient and ApiEVEAuthentication - authentication based on cookies - creating httpclient with url

---
+++ ApiEVE/ApiEVEAuthentication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiEVE
+{
+    //komunitni verze čili jen http
+    public class ApiEVEAuthentication
+    {
+        public async Task<HttpResponseMessage> Authenticate(ApiEVEHttpClient client,string username, string password)
+        {
+            var loginUrl = $"{client.Url}auth/login";
+            var authData = new
+            {
+                username = username,
+                password = password
+            };
+
+            var content = new StringContent(JsonSerializer.Serialize(authData), Encoding.UTF8,"application/json");
+            var response = await client.Client.PostAsync(loginUrl, content);
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> Logout(ApiEVEHttpClient client)
+        {
+            var logoutUrl = $"{client.Url}auth/logout";
+            var response = await client.Client.GetAsync(logoutUrl);
+            return response;
+        }
+    }
+}
---
+++ ApiEVE/ApiEVEHttpClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiEVE
+{
+    public class ApiEVEHttpClient
+    {
+        public string Url { get; private set; }
+        private readonly CookieContainer cookieContainer;
+        public HttpClient Client { get; private set; }
+
+        public ApiEVEHttpClient(string url)
+        {
+            if (url.StartsWith("http://") || url.StartsWith("https://"))
+                this.Url = url.TrimEnd('/') + "/api/";
+            else
+                this.Url = "http://" + url.TrimEnd('/') + "/api/";
+
+            cookieContainer=new CookieContainer();
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                CookieContainer = cookieContainer,
+                UseCookies = true
+            };
+
+            this.Client = new HttpClient(handler)
+            {
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+
+            Client.DefaultRequestHeaders.Add("Accept", "application/json");
+        }
+    }
+}
