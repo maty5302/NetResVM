42af32ebb876d188b0989f4b7723a96ecb12d47f
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-04-01 00:07:50 +0200

    Adding Starting and Stopping labs in EVE-NG and list all nodes from it

--- SuperReservationSystem/Controllers/EVEController.cs
+++ SuperReservationSystem/Controllers/EVEController.cs
@@ -7,6 +7,7 @@ namespace SuperReservationSystem.Controllers
     public class EVEController : Controller
     {
         private readonly ApiEVELabService _apiEVELabService = new ApiEVELabService();
+        private readonly ApiEVENodeService _apiEVENodeService = new ApiEVENodeService();
         private readonly ServerService serverService = new ServerService();
         private readonly UserLabOwnershipService labOwnershipService = new UserLabOwnershipService();
         private readonly UserService userService = new UserService();
@@ -101,6 +102,74 @@ namespace SuperReservationSystem.Controllers
                 return RedirectToAction("LabList", "EVE", new { id = serverId });
             }
             return RedirectToAction("LabList", "EVE", new { id = serverId });
+        }
+
+        public async Task<IActionResult> LabNodeList(int serverId, string labId)
+        {
+            if (labId == null)
+            {
+                TempData["ErrorMessage"] = "Lab not found.";
+                return RedirectToAction("LabList", "EVE", new { id = serverId });
+            }
+            var lab = await _apiEVELabService.GetLabInfoById(serverId, labId);
+            if (lab == null)
+            {
+                TempData["ErrorMessage"] = "";
+                return RedirectToAction("LabList", "EVE", new { id = serverId });
+            }
+            var nodes = await _apiEVENodeService.GetAllNodes(serverId, lab.Filename);
+            ViewBag.Title = lab.Name + " nodes";
+            ViewBag.Nodes = nodes;
+            return View();
+        }
+
+
+        public async Task<IActionResult> StartLab(int serverId, string labId)
+        {
+            if (labId == null)
+            {
+                TempData["ErrorMessage"] = "Lab not found.";
+                return RedirectToAction("LabList", "EVE", new { id = serverId });
+            }
+            var lab = await _apiEVELabService.GetLabInfoById(serverId, labId);
+            if (lab == null)
+            {
+                return RedirectToAction("LabList", "EVE", new { id = serverId });
+            }
+            var res = await _apiEVENodeService.StartAllNodes(serverId, lab.Filename);
+            if (!res)
+            {
+                TempData["ErrorMessage"] = "Couldn't start a lab";
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Lab started";
+            }
+            return RedirectToAction("LabInfo", "EVE", new { id = serverId , labId = labId });
+        }
+
+        public async Task<IActionResult> StopLab(int serverId, string labId)
+        {
+            if (labId == null)
+            {
+                TempData["ErrorMessage"] = "Lab not found.";
+                return RedirectToAction("LabList", "EVE", new { id = serverId });
+            }
+            var lab = await _apiEVELabService.GetLabInfoById(serverId, labId);
+            if (lab == null)
+            {
+                return RedirectToAction("LabList", "EVE", new { id = serverId });
+            }
+            var res = await _apiEVENodeService.StopAllNodes(serverId, lab.Filename);
+            if (!res)
+            {
+                TempData["ErrorMessage"] = "Couldn't start a lab";
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Lab started";
+            }
+            return RedirectToAction("LabInfo", "EVE", new { id = serverId, labId = labId });
         }
     }
 }
