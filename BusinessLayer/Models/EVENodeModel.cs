b00b6f0089deeda8fde8cb6d4fbeb060810cd11d
maty5302 <103773386+maty5302@users.noreply.github.com>
2025-04-01 00:05:43 +0200

    Adding EVE-NG Node model
    Took 49 seconds

---
+++ BusinessLayer/Models/EVENodeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class EVENodeModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+        [JsonPropertyName("cpu")]
+        public int NumberOfCPU {  get; set; }
+        [JsonPropertyName("ram")]
+        public int Memory {  get; set; }
+        [JsonPropertyName("ethernet")]
+        public int NumberOfEthernet { get; set; }
+        [JsonPropertyName("url")]
+        public required string UrlConnect { get; set; }
+
+    }
+}
