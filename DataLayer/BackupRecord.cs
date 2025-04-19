f8700a6fb18a181419d557d15824cb8ce9a1d12f
Matěj Prudil <matej.prudil@outlook.cz>
2025-04-19 12:57:08 +0200

    Adding documentation

--- DataLayer/BackupRecord.cs
+++ DataLayer/BackupRecord.cs
@@ -1,17 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataLayer
+﻿namespace DataLayer
 {
+    /// <summary>
+    /// Represents backup record from local backup storage
+    /// </summary>
     public class BackupRecord
     {
+        /// <summary>
+        /// Gets or sets the type of server (e.g., Cisco, EVE, etc.).
+        /// </summary>
         public string ServerType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the lab for which the backup was created.
+        /// </summary>
         public string LabId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the backup file.
+        /// </summary>
         public string FileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full path to the backup file.
+        /// </summary>
         public string FullPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the backup was created.
+        /// </summary>
         public DateTime CreatedAt { get; set; }
     }
 }
